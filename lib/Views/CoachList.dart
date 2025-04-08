17b785e6bddd8c579e9af1659cd8cb847ee533e9
noorsharkia1 <noor.shar7122007@gmail.com>
2025-04-08 11:54:44 +0300

    Merge remote-tracking branch 'origin/master'

--- lib/Views/CoachList.dart
+++ lib/Views/CoachList.dart
@@ -10,42 +10,52 @@ class CoachList extends StatefulWidget {
 }
 
 class CoachListState extends State<CoachList> {
-  String _selectedTrainerName = "Trainer 1";
+  String _selectedTrainerName = "John Doe";
   String _selectedTrainerImage = "assets/trainer1.jpg";
   String _selectedTrainerFirstName = "John";
   String _selectedTrainerLastName = "Doe";
   int _selectedTrainerAge = 30;
   String _selectedTrainerLocation = "New York";
 
-  // المدربين الآخرين
-  final List<Map<String, String>> otherTrainers = [
-    {
-      'name': 'Trainer 2',
-      'image': 'assets/trainer2.jpg',
-      'firstName': 'Jane',
-      'lastName': 'Smith',
-      'age': '28',
-      'location': 'Los Angeles',
-    },
-    {
-      'name': 'Trainer 3',
-      'image': 'assets/trainer3.jpg',
-      'firstName': 'Emily',
-      'lastName': 'Johnson',
-      'age': '32',
-      'location': 'Miami',
-    },
-    {
-      'name': 'Trainer 4',
-      'image': 'assets/trainer4.jpg',
-      'firstName': 'Mike',
-      'lastName': 'Williams',
-      'age': '35',
-      'location': 'Chicago',
-    },
-  ];
+  // هذه بيانات المدربين تأتي من قاعدة البيانات (على سبيل المثال)
+  Future<List<Map<String, String>>> getTrainers() async {
+    // استبدل هذا الكود بعملية جلب البيانات من قاعدة البيانات.
+    return [
+      {
+        'name': 'John Doe',
+        'image': 'assets/trainer1.jpg',
+        'firstName': 'John',
+        'lastName': 'Doe',
+        'age': '30',
+        'location': 'New York',
+      },
+      {
+        'name': 'Jane Smith',
+        'image': 'assets/trainer2.jpg',
+        'firstName': 'Jane',
+        'lastName': 'Smith',
+        'age': '28',
+        'location': 'Los Angeles',
+      },
+      {
+        'name': 'Emily Johnson',
+        'image': 'assets/trainer3.jpg',
+        'firstName': 'Emily',
+        'lastName': 'Johnson',
+        'age': '32',
+        'location': 'Miami',
+      },
+      {
+        'name': 'Mike Williams',
+        'image': 'assets/trainer4.jpg',
+        'firstName': 'Mike',
+        'lastName': 'Williams',
+        'age': '35',
+        'location': 'Chicago',
+      },
+    ];
+  }
 
-  // لتغيير المدرب
   void _onSelectTrainer(Map<String, String> trainer) {
     setState(() {
       _selectedTrainerName = trainer['name']!;
@@ -57,7 +67,6 @@ class CoachListState extends State<CoachList> {
     });
   }
 
-  // عرض الـ popup عند الضغط على اسم المدرب
   void _showTrainerDetailsPopup(Map<String, String> trainer) {
     showDialog(
       context: context,
@@ -92,7 +101,6 @@ class CoachListState extends State<CoachList> {
     );
   }
 
-  // نافذة التأكيد عند الضغط على "انضمام"
   void _showConfirmationDialog(Map<String, String> trainer) {
     showDialog(
       context: context,
@@ -134,68 +142,88 @@ class CoachListState extends State<CoachList> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // صورة المدرب الحالي في الأعلى
-              CircleAvatar(
-                radius: 80,
-                backgroundImage: AssetImage(_selectedTrainerImage),
-              ),
-              SizedBox(height: 20),
+      body: FutureBuilder<List<Map<String, String>>>(
+        future: getTrainers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
 
-              // تفاصيل المدرب الحالي
-              Text(
-                '$_selectedTrainerFirstName $_selectedTrainerLastName',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'Age: $_selectedTrainerAge',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-              Text(
-                'Location: $_selectedTrainerLocation',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-              SizedBox(height: 30),
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
 
-              // المدربين الآخرين
-              Text(
-                'Other Trainers:',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
+          final trainers = snapshot.data!;
 
-              // عرض صور المدربين الآخرين بشكل أفقي
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: otherTrainers.map((trainer) {
-                    return GestureDetector(
-                      onTap: () => _showTrainerDetailsPopup(trainer),
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 40,
-                            backgroundImage: AssetImage(trainer['image']!),
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // صورة المدرب الحالي في الأعلى
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundImage: AssetImage(_selectedTrainerImage),
+                  ),
+                  SizedBox(height: 20),
+
+                  // تفاصيل المدرب الحالي
+                  Text(
+                    '$_selectedTrainerFirstName $_selectedTrainerLastName',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Age: $_selectedTrainerAge',
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                  Text(
+                    'Location: $_selectedTrainerLocation',
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                  ),
+                  SizedBox(height: 30),
+
+                  // المدربين الآخرين
+                  Text(
+                    'Other Trainers:',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+
+                  // عرض صور المدربين الآخرين بشكل أفقي
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: trainers.map((trainer) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: GestureDetector(
+                            onTap: () => _showTrainerDetailsPopup(trainer),
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: 40,
+                                  backgroundImage: AssetImage(trainer['image']!),
+                                ),
+                                SizedBox(height: 8),
+                                // اسم المدرب
+                                Text(
+                                  trainer['name']!, // عرض الاسم فقط
+                                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                                  textAlign: TextAlign.center, // محاذاة النص في المنتصف
+                                ),
+                              ],
+                            ),
                           ),
-                          SizedBox(height: 10),
-                          Text(
-                            trainer['name']!,
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
